2f66abd7a65408ecc59b11c59039dd68a23a3bab
Talha Acikgoz <tacikgoz58@gmail.com>
2023-12-07 20:26:44 +0300

    [TOO MANY CHANGES] i dont know we changed something

--- __trans__/backdoor/transcendence/transcendence/settings.py
+++ __trans__/backdoor/transcendence/transcendence/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
