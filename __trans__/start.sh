8b8aadef029a25de3033224079f0636363ae5380
Talha Acikgoz <tacikgoz58@gmail.com>
2023-12-12 21:41:26 +0300

    file paths changed

--- __trans__/start.sh
+++ __trans__/start.sh
@@ -2,5 +2,5 @@
 # sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 # sed -i 's/#Port 22/Port 33333/' /etc/ssh/sshd_config
 # service ssh restart
-python3 manage.py runserver 0.0.0.0:6969
-# gunicorn transcendence.wsgi:application --bind 0.0.0.0:6969
+# python3 /transcendence/manage.py runserver 0.0.0.0:8000
+gunicorn transcendence.wsgi -b 0.0.0.0:8000
--- __trans__/transcendence/static/main.js
+++
@@ -1,4 +0,0 @@
-import * as t from './test.js';
-
-t.say();
-
