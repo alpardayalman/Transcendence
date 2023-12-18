aaa5bd21cf9aa53a59917b155e75e2c194c5b376
Eyup kemal Karaoglu <ekaraogl@k2m10s01.42kocaeli.com.tr>
2023-12-18 20:50:22 +0300

    fail

--- __trans__/transcendence/home/views.py
+++ __trans__/transcendence/home/views.py
@@ -1,5 +1,6 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
-# from django.http import HttpResponse
+
 # from django.template import Context, loader
 # from rest_framework_swagger.views import get_swagger_view
 
@@ -9,6 +10,16 @@ def index(request):
     # template = loader.get_template('index.html')
     return render(request, 'index.html')
 
+
 # def schema(request):
 #     schema = get_swagger_view(title='Transcendence API')
 #     return render(request, schema)
+
+#randomstring = ["This is section 1.","This is section 2.", "This is section 3."]
+
+#def section(request, num):
+#    if num <= 3:
+#        return HttpResponse(randomstring[num-1])
+#    else:
+#        raise Http404('No such section')
+
