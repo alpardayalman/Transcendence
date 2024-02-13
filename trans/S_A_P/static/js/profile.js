cd6b13e194e7c963318e35fea55c413c6c8fea4b
eyup kemal <eyupkaraaoglu@gmail.com>
2024-02-14 00:52:52 +0300

    front v2

---
+++ trans/S_A_P/static/js/profile.js
@@ -0,0 +1,34 @@
+console.log('profile.js loaded');
+document.title = 'Profile';
+
+document.addEventListener('DOMContentLoaded', loading);
+window.addEventListener('popstate', loading);
+
+loading();
+
+async function loading() {
+    console.log('profile.js loading');
+    let response = await fetch(window.location.origin + '/api/profile/', {});
+    if (!response.ok) {
+        alert('Error loading profile');
+        return;
+    }
+    let json = await response.json();
+    profile = json.data[0];
+    let card = document.getElementById('card-inner');
+    console.log(json.data[0]);
+    let inner = card.querySelectorAll('.col-sm-9');
+    inner[0].innerText = profile['username'];
+    inner[1].innerText = profile['email'];
+    inner[2].innerText = profile['first_name'];
+    inner[3].innerText = profile['last_name'];
+    inner[4].innerText = profile['is_active'];
+    let userinner = document.getElementById('user');
+    let asd = document.querySelector('#userlinks');
+    let myspan = asd.querySelectorAll('span');
+    myspan[0].innerText = profile['username'];
+    myspan[1].innerText = profile['username'];
+    console.log(json.data[0]);
+    userinner.innerText = profile['username'];
+
+}
