2cb7ee712d366c60ab24d725d52ad8e3df13608a
TalhaAcikgoz <89697506+TalhaAcikgoz@users.noreply.github.com>
2024-03-01 00:09:32 +0300

    Match api improved

--- Transcendence/Api/Match/serializer.py
+++ Transcendence/Api/Match/serializer.py
@@ -10,22 +10,75 @@ class UsernameSerializer(serializers.ModelSerializer):
         fields = ("username",)
 
 
+class MatchYedekSerializer(serializers.Serializer):
+    Winner = serializers.CharField()
+    Loser = serializers.CharField()
+    ScoreOne = serializers.IntegerField()
+    ScoreTwo = serializers.IntegerField()
+    Date = serializers.DateTimeField(read_only=True)
+
+    def validate(self, attrs):
+        if attrs['ScoreOne'] < 0 or attrs['ScoreTwo'] < 0:
+            raise serializers.ValidationError("Score can not be negative")
+        elif attrs['ScoreOne'] == attrs['ScoreTwo']:
+            raise serializers.ValidationError("Score can not be equal")
+        elif attrs['Winner'] == attrs['Loser']:
+            raise serializers.ValidationError("Winner and Loser can not be same")
+        elif not attrs['Winner'] == "guest":
+            if CustomUser.objects.filter(username=attrs['Winner']).count() == 0:
+                raise serializers.ValidationError("Winner does not exist")
+        elif not attrs['Loser'] == "guest":
+            if CustomUser.objects.filter(username=attrs['Loser']).count() == 0:
+                raise serializers.ValidationError("Loser does not exist")
+        return super().validate(attrs)
+    
+    def create(self, validated_data):
+        if validated_data['Winner'] == "guest":
+            guest, _ = CustomUser.objects.get_or_create(username="guest")
+            if validated_data['Loser'] == "guest":
+                return None
+            else:
+                Loser=CustomUser.objects.get(username=validated_data['Loser'])
+                if validated_data['ScoreOne'] > validated_data['ScoreTwo']:
+                    Winner=guest
+                    Loser=Loser
+                else:
+                    Winner=Loser
+                    Loser=guest
+                return Match.objects.create(
+                    Winner=Winner,
+                    Loser=Loser,
+                    ScoreOne=validated_data['ScoreOne'],
+                    ScoreTwo=validated_data['ScoreTwo']
+                )
+        elif validated_data['Loser'] == "guest":
+            pass
+        else:
+            userone=CustomUser.objects.get(username=validated_data['Winner'])
+            usertwo=CustomUser.objects.get(username=validated_data['Loser'])
+            if validated_data['ScoreOne'] > validated_data['ScoreTwo']:
+                winn=userone
+                lose=usertwo
+            else:
+                winn=usertwo
+                lose=userone
+            return Match.objects.create(
+                Winner=winn,
+                Loser=lose,
+                ScoreOne=validated_data['ScoreOne'],
+                ScoreTwo=validated_data['ScoreTwo']
+            )
+
+    # class Meta:
+    #     model = Match
+
 class MatchSerializer(serializers.ModelSerializer):
     Winner = UsernameSerializer()
     Loser = UsernameSerializer()
+    ScoreOne = serializers.IntegerField()
+    ScoreTwo = serializers.IntegerField()
 
-    def is_valid(self, data):
-        print('a= ', data)
-        wine = self.initial_data['Winner.username']
-        print(wine)
-        if wine == "ahmet":
-            raise serializers.ValidationError("User is ahmet mfakirs")
-        return wine
-
-    # def create(self, validated_data):
-    #     user_data = validated_data.pop('user')
-    #     user, _ = CustomUser.objects.get_or_create(**user_data)
-        
+    
 
     class Meta:
         model = Match
--- Transcendence/Api/Match/views.py
+++ Transcendence/Api/Match/views.py
@@ -1,9 +1,27 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import ListAPIView
-from .serializer import MatchSerializer
+from .serializer import MatchSerializer, MatchYedekSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .model import Match
+from Chat.models import CustomUser
+
+class MatchwListAPIView(ListAPIView):
+    serializer_class = MatchYedekSerializer
+    queryset = Match.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        serializer = MatchSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data, status=200)
+
+    def post(self, request, *args, **kwargs):
+        serializer = MatchYedekSerializer(data=request.data)
+        if serializer.is_valid():
+            print('valid ', serializer.validated_data)
+            serializer.save()
+        else:
+            return Response(serializer.errors, status=400)
+        return Response(serializer.data, status=200)
 
 class MatchListAPIView(CreateAPIView):
     serializer_class = MatchSerializer
