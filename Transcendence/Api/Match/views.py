2f868ca5ac0391b06094b4693c9ec1e7ab98c56c
Talha Acikgoz <tacikgoz58@gmail.com>
2024-03-07 17:33:46 +0300

    [match get] is done

--- Transcendence/Api/Match/views.py
+++ Transcendence/Api/Match/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from Api.Match.model import Match
 from Chat.models import CustomUser
 from Api.Match.serializer import MatchGetSerializer, MatchPostSerializer, UsernameSerializer
+import json
 
 class MatchPostAPIView(APIView):
     serializer_class = MatchPostSerializer
@@ -32,6 +33,17 @@ class MatchGetAPIView(APIView):
         except Match.DoesNotExist:
             return Response({'status': '404 instance is not found.'})
         
-        serializer = MatchGetSerializer(instance)
-        return Response({'status': True, 'data': serializer})
-        # return Response({'status': False, 'data': serializer.errors})
+        serializer = MatchGetSerializer(instance, data=request.data)
+        if serializer.is_valid():
+            userOne = CustomUser.objects.get(id=serializer.data['UserOne'])
+            userTwo = CustomUser.objects.get(id=serializer.data['UserTwo'])
+            resp = {
+                "UserOne": userOne.username,
+                "UserTwo": userTwo.username,
+                "ScoreOne": serializer.data['ScoreOne'],
+                "ScoreTwo": serializer.data['ScoreTwo'],
+                "Date": serializer.data['Date']
+            }
+            print(serializer.data)
+            return Response({'status': True, 'data': json.dumps(resp)})
+        return Response({'status': False, 'data': serializer.errors})
