e5e5830e559470d3ae0014defd62bd0e13d3cd56
Eyup kemal Karaoglu <ekaraogl@k2m10s01.42kocaeli.com.tr>
2024-03-14 03:06:21 +0300

    pong game info page add

--- Transcendence/Transcendence/settings.py
+++ Transcendence/Transcendence/settings.py
@@ -12,21 +12,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-zf496afp_*1dtt&otztq1v6r2znsxg!az9#@k%q_gv*dt)s5o='
+#SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-zf496afp_*1dtt&otztq1v6r2znsxg!az9#@k%q_gv*dt)s5o='
 
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+#SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+#DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
